583f04313902e525b8dfa082ffee91c722bafd93
matheussabatine <matheussabatine1717@gmail.com>
2021-10-12 17:41:24 -0300

    botao parte 1

--- LOJA/comando.js
+++ LOJA/comando.js
@@ -23,3 +23,63 @@ $('nav ul li').click(function(){
     $(this).addClass("active").siblings().removeClass("active");
 
 });
+
+/*botao de incremento*/ 
+        //setting default attribute to disabled of minus button
+        document.querySelector(".minus-btn").setAttribute("disabled", "disabled");
+        //taking value to increment decrement input value
+        var valueCount
+        //estoque
+        var estoque
+        //função que imprime valor atual do botão
+        function Botaovalor() {
+            valueCount = document.getElementById("quantity").value;
+            document.getElementById("imprime").innerText = valueCount
+        }
+
+        //plus button
+        document.querySelector(".plus-btn").addEventListener("click", function()
+        {
+            //getting value of input
+            valueCount = document.getElementById("quantity").value;
+            // definindo um estoque
+            estoque = 13
+
+            //input value increment by 1
+            valueCount++;
+
+            //setting increment input value
+            document.getElementById("quantity").value = valueCount
+
+            if (valueCount > 1){
+                document.querySelector(".minus-btn").removeAttribute("disabled");
+                document.querySelector(".minus-btn").classList.remove("disabled")
+            }
+            // fazer condição if que impede que o contador ultrapasse o valor disponível no estoque
+            if (valueCount == estoque){
+                document.querySelector(".plus-btn").setAttribute("disabled", "disabled")
+            }
+        })
+
+        //minus button
+        document.querySelector(".minus-btn").addEventListener("click", function()
+        {
+            //getting value of input
+            valueCount = document.getElementById("quantity").value;
+
+            //input value decrement by 1
+            valueCount--;
+
+            //setting increment input value
+            document.getElementById("quantity").value = valueCount
+
+            if (valueCount == 1){
+                document.querySelector(".minus-btn").setAttribute("disabled", "disabled")
+            }
+            // condição que libera button plus quando é possível adcionar estoque
+            if (valueCount < estoque){
+                document.querySelector(".plus-btn").removeAttribute("disabled");
+                document.querySelector(".plus-btn").classList.remove("disabled")
+            }
+        })
+
